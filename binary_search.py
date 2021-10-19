3c6ee3ba0c6222763883d35757176f28cc1ec1d8
nkleinmann <nekleinmann@gmail.com>
2021-10-18 20:37:06 -0400

    Update solution

--- binary_search.py
+++ binary_search.py
@@ -21,22 +21,4 @@ test_list = [5, 8, 12, 14, 19, 22, 27, 30, 31]
 
 print(binary_search(test_list, 12))
 
-# code your recursive algorithm below here
-
-def recursive_bsearch(data, el):
-    if len(data) == 0:
-        return False
-
-    else: 
-        mid = len(data)//2
-
-        if data[mid] == el:
-            return True
-        else: 
-            if el < data[mid]:
-                return recursive_bsearch(data[:mid], el)
-            else: 
-                return recursive_bsearch(data[mid+1], el)
-
-print(recursive_bsearch(test_list, 1))
 

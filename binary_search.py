8ac7c27b830b4d07ccf6763f6567428f8bae734d
Amii Jiang <amii@192.168.0.107>
2023-04-06 08:58:34 +0800

    first binary algorithm

--- binary_search.py
+++ binary_search.py
@@ -1,2 +1,19 @@
 # code your iterative algorithm here
 
+def binary_search(data, el):
+    first = 0
+    last = len(data)-1
+    found = False
+
+    while first <= last and not found:
+        mid = (first + last) / 2
+        if data[mid] == el:
+            found = True
+        elif el < data[mid]:
+            last = mid - 1
+        else:
+            first = mid + 1
+    return found
+
+test_list = [5,12,14,19,22,27,30,31]
+print(binary_search(test_list,12))
